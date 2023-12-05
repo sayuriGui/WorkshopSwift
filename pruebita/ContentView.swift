0bd5c1e0fb7da52c2ed9669b61795c8c1f5fbc37
Tania Sayuri Guizado Hernandez <A01640092@tec.mx>
2023-12-05 16:55:39 -0600

    Initial Commit

---
+++ pruebita/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pruebita
+//
+//  Created by gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pruebita/pruebitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebitaApp.swift
+//  pruebita
+//
+//  Created by gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct pruebitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
